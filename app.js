751e61e4ed90035ee06033125b2d0a40b3838d30
EC2 Default User <ec2-user@ip-172-31-39-189.us-east-2.compute.internal>
2020-03-21 00:41:23 +0000

    added script, and modified port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = 8080
+const port = process.env.PORT || 8080
 const path = require('path')
 const Task = require('./models/task')
 const toDoList = require('./models/to-do-list')
